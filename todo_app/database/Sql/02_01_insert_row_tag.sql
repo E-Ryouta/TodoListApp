b3fb36744b26ad8603700fe387b32424b2ab6a9f
r.ehara <r.ehara@sint.co.jp>
2024-10-04 01:41:46 +0900

    Insert文を修正しました

--- todo_app/database/Sql/02_01_insert_row_tag.sql
+++ todo_app/database/Sql/02_01_insert_row_tag.sql
@@ -1,50 +1,16 @@
-DELETE FROM "todo_database"."tags"
-
-INSERT INTO "todo_database"."tags"(
+INSERT INTO "tags" (
     "tag_label",
     "tag_color"
 )
-VALUES(
-    "None",
-    "#EEEEEE"
-),
-(
-    "Doc",
-    "#FFFFDD"
-),
-(
-    "Problem",
-    "#FFDDFF"
-),
-(
-    "Bug",
-    "#FFABCE"
-),
-(
-    "Research",
-    "#DDFFFF"
-),
-(
-    "Meeting",
-    "#BAD3FF"
-),
-(
-    "Refactor",
-    "#DCC2FF"
-),
-(
-    "Configuration",
-    "#FFC7AF"
-),
-(
-    "Support",
-    "#B1F9D0"
-),
-(
-    "Test",
-    "#EDFFBE"
-),
-(
-    "Feature",
-    "#C2EEFF"
-)
+VALUES
+    ('None', '#EEEEEE'),
+    ('Doc', '#FFFFDD'),
+    ('Problem', '#FFDDFF'),
+    ('Bug', '#FFABCE'),
+    ('Research', '#DDFFFF'),
+    ('Meeting', '#BAD3FF'),
+    ('Refactor', '#DCC2FF'),
+    ('Configuration', '#FFC7AF'),
+    ('Support', '#B1F9D0'),
+    ('Test', '#EDFFBE'),
+    ('Feature', '#C2EEFF');
